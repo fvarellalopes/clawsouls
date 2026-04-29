20bd2bd3edb301a10c0450e3fe6499e54aa15b14
disconexo <disconexo@openclaw>
2026-04-29 08:51:28 +0800

    chore: add dist/ to gitignore, remove tracked build artifacts

--- dist/_next/static/U3Y5f4sxfxmnI0vd17Jmh/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Feditor","\u002F[locale]\u002Fmy-presets","\u002F[locale]\u002Fpresets"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
